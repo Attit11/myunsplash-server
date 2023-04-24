0646376bf88e71398949d3b1ba11a3e8d4215bb5
Attit11 <sharmaattit09@gmail.com>
2023-04-24 00:31:53 -0400

    wrote a middleware function for authentication

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 require("./src/mongoose/db")
+const cors = require("cors")
 
 const app = express()
 const PORT = process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
 
 
 app.listen(PORT, ()=>{
